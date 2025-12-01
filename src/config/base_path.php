14b4d45110dcc1a8ffcb4d58a204925b97a56d5e
Hamim Alam <hamim.alam06@gmai.com>
2025-12-01 21:13:08 +0000

    - changed base path for infinityfree to work

--- src/config/base_path.php
+++ src/config/base_path.php
@@ -1,4 +1,10 @@
 <?php
 // config.php
-define('BASE_PATH', '/final_task_o_mania-1');
+
+// Automatically set BASE_PATH based on the server environment
+if (strpos($_SERVER['HTTP_HOST'], 'localhost') !== false) {
+    define('BASE_PATH', '/final_task_o_mania-1'); // For localhost
+} else {
+    define('BASE_PATH', '/'); // For InfinityFree or other environments
+}
 ?>
